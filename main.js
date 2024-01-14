8527428774432188c84587a76441b36af016a0f3
AlexanderKochish <shvepsolek@gmail.com>
2024-01-14 16:10:33 +0000

    create buttons

--- main.js
+++ main.js
@@ -27,6 +27,18 @@ document.querySelector('#app').innerHTML = `
   <div class="container">
   <div class="calculator">
   <div class="outputScreen" id="display">0</div>
+  <div>
+  <button>0</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  </div>
   </div>
   </div>
   </div>
