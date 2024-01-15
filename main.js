ca02bc34ba2b0415e5a185792f09b2a51228063c
AlexanderKochish <shvepsolek@gmail.com>
2024-01-15 13:18:22 +0000

    hello 2

--- main.js
+++ main.js
@@ -1,33 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-
-document.querySelector('#app').innerHTML = `
+import "./style.css";
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#app").innerHTML = `
   <div class="wrapper">
     <div class="container">
       <div class="calculator">
-        <div class="outputScreen" id="display">0</div>
-          <div class="btns-container">
+        <div class="outputScreen">
+        <div id="display-results"></div>
+        <div id="display"></div>
+        </div>
+          <div class="btns-container" id="btns-parent">
             <button id="allClear">AC</button>
             <button id="divide">/</button>
             <button id="multiply">*</button>
@@ -51,45 +32,46 @@ document.querySelector('#app').innerHTML = `
       </div>
     </div>
   </div>
-`
+`;
+  let outputScreen = document.querySelector("#display");
+  let buttonsParent = document.querySelector("#btns-parent");
+  let equals = document.querySelector("#equals");
+  let allClear = document.querySelector("#allClear");
+  let displayResults = document.querySelector('#display-results')
 
-let outputScreen = document.querySelector('#display')
-let allButtons = document.querySelectorAll('button')
-let equals = document.querySelector('#equals');
-let allClear = document.querySelector('#allClear')
+  let number = "";
+  let result = "";
+  outputScreen.textContent = "0";
 
-let number = '';
-let result = '';
+  buttonsParent.addEventListener("click", (e) => {
+      if (e.target) {
+        switch (e.target.textContent) {
+          case "=":
+            return "";
+          case "AC":
+            return "";
+          default:
+            (number += e.target.textContent == "=" ? "" : e.target.textContent),
+            (outputScreen.textContent = number);
+          return;
+        }
+      }
+    });
 
-allButtons.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    if(e.target){
-      switch(e.target.textContent){
-        case '=':
-          return '';
-        case 'AC':
-          return '';
-        default:
-          number += e.target.textContent == '='? '':e.target.textContent,
-          outputScreen.textContent = number;
-          return 
-      } 
-    }
-    
-  })
-})
+  function culc(num) {
+    result = String(eval(num));
+    displayResults.textContent = result;
+    return result;
+  }
 
+  equals.addEventListener("click", () => culc(number));
 
-function culc(num){
-  result = String(eval(num))
-  outputScreen.textContent = result;
-  return result;
-}
-equals.addEventListener('click', () => culc(number)) 
+  const clearScreen = () => {
+    number = "";
+    result = "";
+    outputScreen.textContent = "0";
+    displayResults.textContent = "";
+  };
 
-const clearScreen = () => {
-  outputScreen.textContent = '';
-}
-allClear.addEventListener('click', clearScreen)
-
-setupCounter(document.querySelector('#counter'))
+  allClear.addEventListener("click", clearScreen);
+});
